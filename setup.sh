59830bf65d21bb6065a415550af5720074042a7e
Timo Heister <timo.heister@gmail.com>
2015-08-04 10:00:22 -0500

    initial version

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ ! -d "hashdist" ]; then
+  git clone https://github.com/hashdist/hashdist.git
+fi
+if [ ! -d "hashstack" ]; then
+  git clone https://github.com/hashdist/hashstack.git
+fi
+
+./hashdist/bin/hit build -j 4 --verbose
